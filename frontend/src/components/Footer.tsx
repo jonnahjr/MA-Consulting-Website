7877a7002ba82acc9cf2732cd2762ae8c1581ba1
yonas bogale <pronivamusic14@gmail.com>
2025-10-02 03:49:32 -0700

    Delete frontend directory

--- frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer(){
-  return (
-    <footer className="w-full mt-12 border-t border-white/6">
-      <div className="max-w-7xl mx-auto px-6 py-8 text-sm text-white/80">© {new Date().getFullYear()} MA Services. All rights reserved.</div>
-    </footer>
-  )
-}
